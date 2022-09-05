755963b5f33dc7f7c9dd3842d50409cbe2e05b5f
mightysidhe <a.r.kyselova@gmail.com>
2022-09-05 12:10:34 +0300

    Updated time, pulled temperature

--- src/index.js
+++ src/index.js
@@ -16,18 +16,34 @@ let days = [
 
 let currentDay = days[now.getDay()];
 let hours = now.getHours();
-
 let minutes = now.getMinutes();
 
 day.innerHTML = `${currentDay}`;
+if (hours < 10) {
+  hours = `0${hours}`;
+}
+if (minutes < 10) {
+  minutes = `0${minutes}`;
+}
 time.innerHTML = `${hours}:${minutes}`;
+
+function showTemperature(response) {
+  let temperatureRounded = Math.round(response.data.main.temp);
+  let currentTemperature = document.querySelector(".temperature");
+  currentTemperature.innerHTML = `${temperatureRounded}°C`;
+}
 
 function searchCity(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#city-input");
-
   let city = document.querySelector(".city");
   city.innerHTML = `${searchInput.value}`;
+  let units = "metric";
+  let apiKey = "de2c40e370d58e257faf07ba4ea95840";
+  let apiEndPoint = "https://api.openweathermap.org/data/2.5/weather";
+  let apiUrl = `${apiEndPoint}?q=${searchInput.value}&appid=${apiKey}&units=${units}`;
+
+  axios.get(`${apiUrl}&appid=${apiKey}&units=metric`).then(showTemperature);
 }
 
 let search = document.querySelector("#search-form");
