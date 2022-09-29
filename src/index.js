759c21f2cb98cd6e40b1a917accd9a5970cbe385
mightysidhe <a.r.kyselova@gmail.com>
2022-09-29 20:27:52 +0300

    Changed the name of a day

--- src/index.js
+++ src/index.js
@@ -30,7 +30,7 @@ time.innerHTML = `${hours}:${minutes}`;
 function formatDay(timestamp) {
   let date = new Date(timestamp * 1000);
   let day = date.getDay();
-  let days = ["Sun", "Mon", "Tue", "Wen", "Thu", "Fri", "Sun"];
+  let days = ["Sun", "Mon", "Tue", "Wen", "Thu", "Fri", "Sat"];
 
   return days[day];
 }
